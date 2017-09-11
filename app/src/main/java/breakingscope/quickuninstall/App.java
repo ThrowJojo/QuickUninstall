edaad525fbf231ac52e2aeeeabb9fd8c281aeb7b
Jordan Holland <thisrememo@gmail.com>
2017-09-11 10:51:12 +1000

    Added Fabric for Analytics.

--- app/src/main/java/breakingscope/quickuninstall/App.java
+++ app/src/main/java/breakingscope/quickuninstall/App.java
@@ -1,12 +1,15 @@
 package breakingscope.quickuninstall;
 
 import android.app.Application;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
     }
 
 }
